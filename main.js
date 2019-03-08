a64907cb00f86c72bc9f677f0493dfc1b3e5ae73
Ngando epoh herve <epohherve63@gmail.com>
2019-03-08 01:01:11 +0100

    resume
    resume

---
+++ main.js
@@ -0,0 +1,54 @@
+/** follow mouse fonction */
+var $=document.querySelector.bind(document);
+var $on=document.addEventListener.bind(document);
+
+var xmouse,ymouse;
+$on('mousemove',function (e) {
+    xmouse=e.clientX || e.pageX;
+    ymouse=e.clientY || e.pageY;
+});
+
+var ball=$('#ball');
+var x=void 0,
+    y=void 0,
+    dx=void 0,
+    dy=void 0,
+    tx=0,
+    ty=0,
+    key=-1;
+var followMouse=function followMouse() {
+    key=requestAnimationFrame(followMouse);
+    if(!x || !y){
+        x=xmouse;
+        y=ymouse;
+    }else{
+        dx=(xmouse-x)*0.125;
+        dy=(ymouse-y)-0.125;
+        if(Math.abs(dx)+Math.abs(dy)<0.1){
+            x=xmouse;
+            y=ymouse;
+        }else{
+            x+=dx;
+            y+=dy;
+        }
+    }
+    ball.style.left=x+'px';
+    ball.style.top=y+'px';
+};
+
+
+/******************************************* */
+document.getElementById("click").onclick=function () {
+    alert('You have just click me !');
+}
+document.getElementById('click').onmouseover=function(){
+    document.getElementById('click').style.backgroundColor="#000";
+    document.getElementById('click').style.color="#fff";
+    this.style.width="500px";
+}
+
+/************************ */
+document.querySelector('.project').onmouseover=function () {
+    document.querySelector('.project-description').style.visibility='visible';
+}
+ 
